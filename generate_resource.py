318cb742e5f4de59cb20d5de1a51cc739543b726
filipp <Philipoff>
2022-07-30 03:47:27 +0300

    Resource generate

---
+++ generate_resource.py
@@ -0,0 +1,59 @@
+import os
+
+import xml.etree.ElementTree as ET
+
+resource_name = input()
+try:
+    os.makedirs("resources")
+except Exception as e:
+    pass
+
+try:
+    os.makedirs(f"resources/{resource_name}")
+except Exception as e:
+    pass
+
+
+with open(f"resources/{resource_name}/{resource_name}.cs", "w+") as f:
+    f.write("""using GTANetworkAPI;
+
+namespace %s
+{
+public class Main : Script
+    {
+
+    }
+}
+""" % resource_name)
+
+with open(f"resources/{resource_name}/{resource_name}.cproj", "w+") as f:
+    f.write(r"""<Project Sdk="Microsoft.NET.Sdk">
+    <PropertyGroup>
+        <TargetFramework>netcoreapp3.1</TargetFramework>
+        <Configurations>Debug</Configurations>
+        <Platforms>x64</Platforms>
+    </PropertyGroup>
+
+    <ItemGroup>
+      <Reference Include="Bootstrapper">
+        <HintPath>..\..\runtime\Bootstrapper.dll</HintPath>
+      </Reference>
+    </ItemGroup>
+</Project>    
+""")
+
+with open(f"resources/{resource_name}/meta.xml", "w+") as f:
+    f.write(r"""<meta>
+    <info name="%s" type="script"/>
+    <script src="bin/x64/Debug/netcoreapp3.1/<newResource>.dll"/>
+</meta>
+""" % resource_name)
+
+
+tree = ET.parse("dotnet/settings.xml")
+root = tree.getroot()
+
+resource = root.find("resource")
+resource.set("src", resource_name)
+
+tree.write("dotnet/settings.xml")
