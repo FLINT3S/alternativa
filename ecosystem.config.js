675cee3298c61d00e6799a8a5a2e506fd2a477ff
siailya <sia.ilya.dev@gmail.com>
2022-07-31 20:11:27 +0300

    Фикс воркфлоу и файл ecosystem.config.js для pm2 с вотчером

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+    apps: [{
+        script: "./rage-server",
+        name: "alternativa",
+        watch: ["dotnet", "client_packages"],
+        watch_delay: 10000,
+        ignore_watch : ["node_modules", "client/img"],
+    }]
+}
