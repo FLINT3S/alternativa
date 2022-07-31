8d10568ae9108a33aa652c24e517bd3fd34151da
siailya <sia.ilya.dev@gmail.com>
2022-07-31 20:19:24 +0300

    Фикс воркфлоу - обновил бранч для апдейта на dev

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,10 @@
 ﻿module.exports = {
     apps: [{
-        script: "./rage-server",
+        script: "./ragemp-server",
         name: "alternativa",
         watch: ["dotnet", "client_packages"],
         watch_delay: 10000,
         ignore_watch : ["node_modules", "client/img"],
+        max_memory_restart: '500M'
     }]
 }
