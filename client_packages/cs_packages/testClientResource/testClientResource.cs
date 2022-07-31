4ee6a00fc0989834d2a5fc484069a72a68ceeb9d
siailya <sia.ilya.dev@gmail.com>
2022-07-31 07:34:47 +0300

    Sample client package

---
+++ client_packages/cs_packages/testClientResource/testClientResource.cs
@@ -0,0 +1,57 @@
+﻿using RAGE;
+using RAGE.Game;
+using RAGE.Ui;
+
+namespace testClientResource
+{
+    public class Main : Events.Script
+    {
+        private static HtmlWindow browser;
+
+        public Main()
+        {
+            Events.OnPlayerReady += OnPlayerReady;
+            
+            Events.Add("CEF:CLIENT:Test", OnTest);
+            
+            Input.Bind(VirtualKeys.F2, true, OnToggleBrowser);
+        }
+
+        private void OnTest(object[] args)
+        {
+            Chat.Output("Альтернатива - лучший сервер");
+            Chat.Output(args.ToString());
+        }
+
+        private void OnPlayerReady()
+        {
+            Chat.Output("testClientResource123");
+        }
+
+        private void OnCreateBrowser()
+        {
+            browser = new HtmlWindow("package://web/index.html");
+            browser.Active = true;
+            Cursor.ShowCursor(true, true);
+        }
+        
+        private void OnDestroyBrowser()
+        {
+            browser.Active = false;
+            browser.Destroy();
+            Cursor.ShowCursor(false, false);
+        }
+
+        private void OnToggleBrowser()
+        {
+            if (browser == null || !browser.Active)
+            {
+                OnCreateBrowser();
+            }
+            else
+            {
+                OnDestroyBrowser();
+            }
+        }
+    }
+}
