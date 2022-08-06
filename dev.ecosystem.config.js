2aa3ea7e6fd07d7535f709a966d8c467f87dd9b0
siailya <sia.ilya.dev@gmail.com>
2022-08-06 22:11:03 +0300

    Тест и конфиг для pm2

---
+++ dev.ecosystem.config.js
@@ -0,0 +1,8 @@
+﻿module.exports = {
+    apps: [{
+        script: "./ragemp-server.exe",
+        name: "alternativa",
+        watch: ["dotnet", "client_packages"],
+        ignore_watch: ["\.git", ".*\.git*", "logs", ".*\.listcache", ".*\/obj", ".*\/node_modules", ".listcache", "\.listcache", ".git", "client_packages/.listcache"]
+    }]
+}
