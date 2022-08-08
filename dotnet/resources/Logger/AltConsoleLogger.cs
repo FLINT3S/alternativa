e9d16fa18fd5493fba7aa6aae71f1855072899a0
siailya <sia.ilya.dev@gmail.com>
2022-08-08 22:16:01 +0300

    Раскарсил консоль по цветам

--- dotnet/resources/Logger/AltConsoleLogger.cs
+++ dotnet/resources/Logger/AltConsoleLogger.cs
@@ -8,7 +8,33 @@ namespace Logger
     {
         public override Task Log(LogLevel level, AltAbstractEvent serverAltAbstractEvent) =>
             Task.Factory.StartNew(() =>
-                Console.WriteLine(GetLogString(level, serverAltAbstractEvent))
+                {
+                    SetConsoleColor(level);
+                    Console.WriteLine(GetLogString(level, serverAltAbstractEvent));
+                    SetConsoleColor(LogLevel.Info);
+                }
             );
+
+        public void SetConsoleColor(LogLevel level)
+        {
+            switch (level)
+            {
+                case LogLevel.Critical:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogLevel.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogLevel.Development:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case LogLevel.Resource:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+        }
     }
 }
