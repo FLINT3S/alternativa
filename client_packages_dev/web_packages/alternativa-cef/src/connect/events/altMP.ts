6ce0a88ae16c468808943729c32cfb51a9735f95
siailya <sia.ilya.dev@gmail.com>
2022-08-13 21:00:49 +0300

    Бридж между CEF и сервером - отправка событий из CEF

--- client_packages_dev/web_packages/alternativa-cef/src/connect/events/altMP.ts
+++ client_packages_dev/web_packages/alternativa-cef/src/connect/events/altMP.ts
@@ -9,6 +9,7 @@ import {EventString} from "@/connect/events/eventString";
 
 window.altListeners = new Map<string, Array<AltEventCallback>>();
 
+type eventFrom = "SERVER" | "CLIENT";
 
 /**
  * @class altMP
@@ -25,23 +26,32 @@ export class altMP extends ModuleDependent {
     const es = new EventString("CEF", "CLIENT", this.moduleName, eventName)
     new AltEvent(es, AltEventType.SEND, data)
 
-    mp.trigger(eventString, ...data)
+    mp.trigger(eventString, data)
   }
 
-  triggerServer(eventString: string, data: object) {
-    const es = new EventString("CEF", "SERVER", this.moduleName, eventString)
+  /**
+   * @method callServer
+   *
+   * Отправляет событие серверу
+   * */
+  triggerServer(eventName: string, data?: object) {
+    const es = new EventString("CEF", "SERVER", this.moduleName, eventName)
     new AltEvent(es, AltEventType.SEND, data)
 
-    mp.trigger(eventString, ...data)
+    mp.trigger("CEF:SERVER", es.eventString, data)
+  }
+
+  triggerServerRawEvent(eventString: string, data?: object) {
+    mp.trigger("CEF:SERVER", eventString, data)
   }
 
   /**
    * @method on
    *
-   * Добавляет обработчик события
+   * Добавляет обработчик события от клиента
    * */
-  on(eventName: string, callback: (...data: any) => void) {
-    const es = new EventString("CLIENT", "CEF", this.moduleName, eventName)
+  on(eventName: string, callback: (...data: any) => void, eventFrom: eventFrom = "CLIENT") {
+    const es = new EventString(eventFrom, "CEF", this.moduleName, eventName)
     new AltEvent(es, AltEventType.REGISTER_LISTENER)
 
     const currentListeners = window.altListeners.get(es.eventString)
@@ -52,18 +62,20 @@ export class altMP extends ModuleDependent {
     }
   }
 
+  /**
+   * @method onServer
+   *
+   * Добавляет обработчик события сервера
+   * */
   onServer(eventName: string, callback: (...data: any) => void) {
-    const es = new EventString("SERVER", "CEF", this.moduleName, eventName)
-    new AltEvent(es, AltEventType.REGISTER_LISTENER)
-
-    const currentListeners = window.altListeners.get(es.eventString)
-    if (!currentListeners) {
-      window.altListeners.set(es.eventString, [callback])
-    } else {
-      window.altListeners.set(es.eventString, [...currentListeners, callback])
-    }
+    this.on(eventName, callback, "SERVER")
   }
 
+  /**
+   * @method onClient
+   *
+   * Фунция для вызова локального события. Используется на клиенте для триггера событий через execute
+   */
   call(eventString: string, data: object) {
     altLog.warning(eventString)
 
