3f47e29e9157a90983aae10c4deaaefcb9e89095
siailya <sia.ilya.dev@gmail.com>
2022-08-14 23:49:20 +0300

    Добавил в авторизацию новые ивенты

--- dotnet/resources/Authorization/Authorization.cs
+++ dotnet/resources/Authorization/Authorization.cs
@@ -1,9 +1,21 @@
+using System.Diagnostics.Tracing;
 using AbstractResource;
+using GTANetworkAPI;
+using Logger;
+using Logger.EventModels;
+
+/*
+ * wiki: https://www.notion.so/Authorization-44a4b5377f2848c59d1772d89dde092d
+ */
 
 namespace Authorization
 {
-    public class Main : AltAbstractResource
+    public class Authorization : AltAbstractResource
     {
-        
+        [ServerEvent(Event.PlayerConnected)]
+        private void OnPlayerConnected(Player player)
+        {
+            AltLogger.Instance.LogInfo(new AltPlayerEvent(player.Name, this, "Player connected", $"Player {player.Name}[{player.SocialClubId}] connected"));
+        }
     }
 }
---
+++ dotnet/resources/Authorization/AuthorizationEvents.cs
@@ -0,0 +1,13 @@
+﻿using AbstractResource;
+
+/*
+ * wiki: https://www.notion.so/Authorization-44a4b5377f2848c59d1772d89dde092d
+ */
+
+namespace Authorization
+{
+    public class AuthorizationEvents : AltAbstractResourceEvents
+    {
+        public const string LoginAttemptFromCef = "CEF:SERVER:Authorization:LoginSubmit";
+    }
+}
