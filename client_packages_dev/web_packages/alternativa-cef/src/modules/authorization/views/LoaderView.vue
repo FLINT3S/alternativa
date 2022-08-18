7f77b8c5118ebc20a14b705daa331cb35475b1c8
siailya <sia.ilya.dev@gmail.com>
2022-08-18 07:01:48 +0300

    Компонент загрузочного экрана

---
+++ client_packages_dev/web_packages/alternativa-cef/src/modules/authorization/views/LoaderView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Loading...
+  </div>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "LoaderView"
+})
+</script>
+
+<style scoped>
+
+</style>
