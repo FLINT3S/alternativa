ac26206d7f96c5f665e6e7c5aef6981750ac8654
siailya <sia.ilya.dev@gmail.com>
2022-08-20 16:40:00 +0300

    Тесты нагрузки

--- dotnet/resources/AdminPanel/AdminPanel.cs
+++ dotnet/resources/AdminPanel/AdminPanel.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 using AbstractResource;
 using GTANetworkAPI;
+using NAPIExtensions;
 
 namespace AdminPanel
 {
@@ -10,11 +13,16 @@ namespace AdminPanel
         [RemoteEvent(AdminPanelEvents.RandomDamageFromCef)]
         private void ReRandomDamage(Player player)
         {
+            player.Health -= (int)Math.Round(new Random().NextDouble() * 20);
             var es = GetEsFromAttr(MethodBase.GetCurrentMethod());
-            
-            player.Health -= (int) Math.Round(new Random().NextDouble() * 20);
+            Task.Run(() =>
+            {
+                var acc = player.GetAccount();
+                acc.UpdateEmail((Int32.Parse(acc.Email) + 1).ToString());
+                Console.WriteLine(acc.Email);
+            });
         }
-        
+
         [Command("testbr")]
         public void CMDOnTestBR(Player player)
         {
