ca109cd0fbcaeda0389d676c4cebbf59b97bce2b
textu <texturich@gmail.com>
2022-08-28 10:20:50 +0300

    небольшие перестановки

--- dotnet/resources/Authorization/ChainsOfResponsibility/PlayerConnectedHandlers/AuthorizationEvents.cs
+++ dotnet/resources/Authorization/ChainsOfResponsibility/PlayerConnectedHandlers/AuthorizationEvents.cs
@@ -2,13 +2,13 @@
 {
     internal static class AuthorizationEvents
     {
-        public const string PermanentlyBanned = "SERVER:CLIENT:Authorization:PermanentBan";
-
-        public const string TemporaryBanned = "SERVER:CLIENT:Authorization:TempBan";
-
         public const string FirstConnectionToClient = "SERVER:CLIENT:Authorization:FirstConnection";
 
         public const string NeedLoginToClient = "SERVER:CLIENT:Authorization:NeedLogin";
+
+        public const string TemporaryBanned = "SERVER:CLIENT:Authorization:TempBan";
+        
+        public const string PermanentlyBanned = "SERVER:CLIENT:Authorization:PermanentBan";
 
         public const string LoginSuccessToClient = "SERVER:CLIENT:Authorization:LoginSuccess";
         
