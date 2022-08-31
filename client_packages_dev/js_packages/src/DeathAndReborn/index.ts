bf033dc57cad7662b915859aa5f4e6a9ec4618a6
Anton <texturich@gmail.com>
2022-08-31 13:24:56 +0300

    приделал звук смерти и комманду /killme

--- client_packages_dev/js_packages/src/DeathAndReborn/index.ts
+++ client_packages_dev/js_packages/src/DeathAndReborn/index.ts
@@ -1,4 +1,5 @@
 mp.events.add("SERVER:CLIENT:DeathAndReborn:Death", () => {
+    mp.game.audio.playSound(-1, "ScreenFlash", "WastedSounds", true, 0, true)
     mp.game.graphics.startScreenEffect("DeathFailMPIn", 300_000, false)
 })
 
