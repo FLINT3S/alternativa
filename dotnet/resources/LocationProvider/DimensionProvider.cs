b13cfc0e593456128c1f2a917d77c9c9a044605d
Anton <texturich@gmail.com>
2022-09-28 22:55:21 +0300

    удалил простые методы

--- dotnet/resources/LocationProvider/DimensionProvider.cs
+++ dotnet/resources/LocationProvider/DimensionProvider.cs
@@ -10,35 +10,26 @@ namespace LocationProvider
         
         private static readonly List<Dimension> BusyDimensions = new List<Dimension>();
 
-        private static void TakeDimension(Dimension dimension) =>
-            BusyDimensions.Add(dimension);
-
         public static uint GetFreeDimension()
         {
             for (uint i = 1; i < uint.MaxValue; i++)
-                if (!BusyDimensions.ContainsDimensionWithThisId(i))
+                if (BusyDimensions.All(d => d != i))
                 {
-                    TakeDimension(new Dimension(i));
+                    BusyDimensions.Add(new Dimension(i));
                     return i;
                 }
 
             throw new InvalidOperationException("All dimensions are busy");
         }
 
-        private static bool ContainsDimensionWithThisId(this IEnumerable<Dimension> dimensions, uint id) =>
-            dimensions.Any(d => d == id);
-
         public static void ReleaseUnusedDimensions()
         {
             foreach (var dimension in GetUnusedDimensions())
-                OnDimensionRelease(dimension);
+                BusyDimensions.Remove(dimension);
         }
 
         private static IEnumerable<Dimension> GetUnusedDimensions() => BusyDimensions.Where(dimension =>
             dimension.Players.Count == 0 && DateTime.Now - dimension.CreatedAt == TimeSpan.FromMinutes(5)
         );
-
-        private static void OnDimensionRelease(Dimension dimension) =>
-            BusyDimensions.Remove(dimension);
     }
 }
