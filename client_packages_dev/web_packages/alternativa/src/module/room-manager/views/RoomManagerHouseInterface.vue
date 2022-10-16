e425ff6b2bbdd109455606b5cdcc4317a25d10ee
Anton <texturich@gmail.com>
2022-10-16 23:35:00 +0300

    гетер инфы о доме, сохранение на улице

--- client_packages_dev/web_packages/alternativa/src/module/room-manager/views/RoomManagerHouseInterface.vue
+++ client_packages_dev/web_packages/alternativa/src/module/room-manager/views/RoomManagerHouseInterface.vue
@@ -1,5 +1,6 @@
 <template>
   <n-card class="width-lg" title="Интерфейс дома">
+    <p>{{ JSON.stringify(houseData) }}</p>
     <n-button @click="enterToHouse">
       Войти в дом '{{houseId}}'
     </n-button>
@@ -10,15 +11,29 @@
 import {storeToRefs} from "pinia";
 import {useRoomManagerStore} from "@/store/roomManager";
 import {useRouter} from "vue-router";
+import {ref} from "vue";
+import {parseJson} from "@/data/parseJson";
 
 const {altMpRoomManager} = storeToRefs(useRoomManagerStore());
 
 const router = useRouter();
-const houseId = router.currentRoute.value.params.houseId;
+const houseId = router.currentRoute.value.params.houseId as string;
 
 const enterToHouse = () => {
   altMpRoomManager.value.triggerServer("EnterToHouse", houseId as string);
 }
+
+const houseData = ref(null)
+
+const loadHouseData = () => {
+  altMpRoomManager.value.triggerServerWithAnswerPending("GetHouseData", houseId)
+      .then(([houseDataJson]) => {
+        houseData.value = JSON.parse(houseDataJson as string)
+      })
+}
+
+loadHouseData()
+
 </script>
 
 <style scoped>
